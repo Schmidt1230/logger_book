111ea3b49c272ea4c17717dbd40f2eb771c6361a
Stephan <143713572+Schmidt1230@users.noreply.github.com>
2024-08-11 17:06:53 +0200

    Started NodeJS Backend
    Started a server.js file which starts a connection with MongoDB. The connection string is added inside an env file which is then ignored by Git.

---
+++ back_end/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const app = express();
+
+
+dotenv.config();
+
+
+async function connect(){
+    try {
+        await mongoose.connect(process.env.URI)
+        console.log("Successfully connected=> Mongoose")
+    }
+    catch (error){
+        console.error(error);}
+}
+
+connect();
+app.listen(8000, () =>{
+    console.log("Sever starter Port=>8000");
+});
+
